3b0cbdfc9715d24e06106a9ff2d1980a78458616
sajeeb23 <sajeebsharma24@gmail.com>
2023-09-15 05:46:03 +0600

    Remaining Credit Hours  and Toast added

--- src/components/courses/Cart.jsx
+++ src/components/courses/Cart.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 
-const Cart = ({selectedCourse}) => {
+const Cart = ({selectedCourse, remaining}) => {
     
     console.log(selectedCourse);
 
     return (
-        <div className="w-96 mt-20">
-            <h1 className="text-4xl font-bold text-[#2F80ED] mb-4">Credit Hour Remaining {} hr</h1>
-            <hr/>
+        <div className="w-96 mt-20 bg-white shadow-xl p-12">
+            <h1 className="text-4xl font-bold text-[#2F80ED] mb-4">Credit Hour Remaining {remaining} hr</h1>
+            <hr className="my-4"/>
             <h1 className="text-4xl font-bold my-4">Course Name</h1>
             <ol style={{ listStyleType: 'decimal' }}>
                  {selectedCourse.map((course) => (
@@ -19,9 +19,9 @@ const Cart = ({selectedCourse}) => {
             </ol>
 
 
-            <hr />
+            <hr className="my-4" />
             <p className="font-bold text-3xl my-3">Total Credit Hour : </p>
-            <hr />
+            <hr className="my-4" />
             <p className="font-bold text-3xl my-3">Total Price :  USD</p>
         </div>
     );
--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,61 +1,74 @@
-/* eslint-disable react/jsx-key */
-import { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { BiBookOpen } from 'react-icons/bi';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast'
 import Cart from "./cart";
 
-
 const Courses = () => {
+  const [allCourses, setAllCourses] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState([]);
+  const [remaining, setRemaining] = useState([20]);
 
-    const[allCourses, setAllCourses] = useState([]);
-    const[selectedCourse, setSelectedCourse] = useState([]);
+  useEffect(() => {
+    fetch("../../../public/Course.json")
+      .then((res) => res.json())
+      .then((data) => setAllCourses(data));
+  }, []);
 
-    useEffect(()=>{
-        fetch("../../../public/Course.json")
-        .then((res) => res.json())
-        .then((data) => setAllCourses(data));
-
-    },[]);
-    const handleSelect = (course) =>{
-        const isExist = selectedCourse.find(item => item.id == course.id);
-        if (isExist){
-            toast.error('Course is already selected!');
-        }
-        else{
-        setSelectedCourse([...selectedCourse, course]);
-        }
+  const calculateTotalCredit = (courses) => {
+    let totalCredit = 0;
+    for (const course of courses) {
+      totalCredit += course.credit;
     }
-    
-    return (
-        <div className="flex">
-        <div className="container mx-auto grid grid-cols-3"> 
-           {
-            allCourses.map(course=>(
-                <div key={course.id} className="mt-20">
-                <div className="card w-80 h-full bg-white shadow-xl">
-                     <figure className="bg-white w-full pt-4 mx-auto"><img src={course.image} alt="Shoes" /></figure>
-                <div className=" card-body bg-white">
-                    <h2 className="card-title font-bold bg-white">{course.title}</h2>
-                    <p className="bg-white">{course.description}</p>
+    return totalCredit;
+  };
+
+  const handleSelect = (course) => {
+    const isExist = selectedCourse.find((item) => item.id === course.id);
+    const totalCredit = calculateTotalCredit(selectedCourse);
+
+    if (isExist) {
+      toast.error('Course is already selected!');
+    } else {
+      if (totalCredit + course.credit <= 20) {
+        setSelectedCourse([...selectedCourse, course]);
+        setRemaining(20 - (totalCredit + course.credit));
+        toast.success('Course added to the cart!');
+      } else {
+        toast.error('Already used 20 credit hours!');
+      }
+    }
+  };
+
+  return (
+    <div className="flex">
+      <div className="container mx-auto grid grid-cols-3">
+        {allCourses.map((course) => (
+          <div key={course.id} className="mt-20">
+            <div className="card w-80 h-full bg-white shadow-xl">
+              <figure className="bg-white w-full pt-4 mx-auto"><img src={course.image} alt="Shoes" /></figure>
+              <div className="card-body bg-white">
+                <h2 className="card-title font-bold bg-white">{course.title}</h2>
+                <p className="bg-white">{course.description}</p>
                 <div className="flex text-center text-lg">
-                    <p className="flex items-center gap-2 bg-white"><BsCurrencyDollar className="bg-white"/>Price : {course.price}</p>
-                    <p className="flex items-center gap-2 bg-white"><BiBookOpen className="bg-white"/>Credit : {course.credit}</p>
+                  <p className="flex items-center gap-2 bg-white"><BsCurrencyDollar className="bg-white" />Price : {course.price}</p>
+                  <p className="flex items-center gap-2 bg-white"><BiBookOpen className="bg-white" />Credit : {course.credit}</p>
                 </div>
                 <div className="card-actions justify-center">
-                     <button onClick={()=>handleSelect(course)} className="btn btn-primary bg-[#2F80ED] w-full font-semibold text-white">Select</button>
-                    </div>
-                    </div>
-                    </div>
+                  <button onClick={() => handleSelect(course)} className="btn btn-primary bg-[#2F80ED] w-full font-semibold text-white">Select</button>
                 </div>
-            ))
-           }
-        </div>
-        <div className="w-96">
-            <Cart  selectedCourse={selectedCourse}></Cart>
-        </div>
-        </div>
-    );
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-96">
+        <Cart selectedCourse={selectedCourse} remaining={remaining} />
+      </div>
+      <Toaster position="top-right" /> 
+    </div>
+  );
 };
 
 export default Courses;
